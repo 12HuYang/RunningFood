5e6a6678833c8717c628d49eff31e2ad95234aaf
12HuYang <yang.hu@wsu.edu>
2017-02-04 17:12:17 -0800

    foodinput

--- foodresponse.php
+++ foodresponse.php
@@ -3,5 +3,32 @@
  * Created by PhpStorm.
  * User: yanghu
  * Date: 2/4/17
- * Time: 10:13 AM
+ * Time: 3:27 PM
  */
+$restraunrant = isset($_POST['Restaurant'])? $_POST['Restaurant'] : '';
+//echo $restraunrant;
+
+//$foodname = isset($_POST['FoodName'])? $_POST['FoodName'] : '';
+//echo $foodname;
+
+//$price = isset($_POST['Price'])? $_POST['Price'] : '';
+//echo $price;
+//$filename = time().substr($_FILES['photo']['name'], strrpos($_FILES['photo']['name'],'.'));
+$filename=$_FILES['photo']['name'];
+
+
+
+$response = array();
+
+if(move_uploaded_file($_FILES['photo']['tmp_name'], $filename)){
+    $response['isSuccess'] = true;
+    $response['restraunt'] = $restraunrant;
+    //$response['foodname'] = $foodname;
+    //$response['price']=$price;
+    $response['photo'] = $filename;
+}else{
+    $response['isSuccess'] = false;
+}
+
+echo json_encode($response);
+?>
--- loadpic.php
+++
@@ -1,2 +0,0 @@
-<form name="upfile_form" method="post" action="upload.php" enctype="multipart/form-data">
-    <input type="file" name="upimages"><input type="submit" value="upload">
